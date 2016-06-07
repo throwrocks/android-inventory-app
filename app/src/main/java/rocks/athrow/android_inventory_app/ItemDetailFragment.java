baa4d35361acf6b4cf3b0b2cf06a23d1f27153a7
Jose E Lopez <p0p.thr0w@gmail.com>
2016-06-06 21:55:23 -0500

    Create methods for all button onClickListeners.

--- app/src/main/java/rocks/athrow/android_inventory_app/ItemDetailFragment.java
+++ app/src/main/java/rocks/athrow/android_inventory_app/ItemDetailFragment.java
@@ -1,6 +1,7 @@
 package rocks.athrow.android_inventory_app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,8 +14,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -45,7 +48,7 @@ public class ItemDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getActivity().getIntent();
-        itemId = intent.getIntExtra("item_id",0);
+        itemId = intent.getIntExtra("item_id", 0);
 
         RealmConfiguration realmConfig = new RealmConfiguration.Builder(getContext()).build();
         Realm.setDefaultConfiguration(realmConfig);
@@ -66,7 +69,6 @@ public class ItemDetailFragment extends Fragment {
         itemVendorEmail = item.getVendorEmail();
 
 
-
     }
 
     @Override
@@ -80,24 +82,102 @@ public class ItemDetailFragment extends Fragment {
         TextView itemVendorNameView = (TextView) rootView.findViewById(R.id.item_detail_vendor_name);
         TextView itemVendorEmailView = (TextView) rootView.findViewById(R.id.item_detail_vendor_email);
 
+        Button itemQuantityAddButton = (Button)  rootView.findViewById(R.id.item_quantity_add);
+        Button itemQuantityRemoveButton = (Button)  rootView.findViewById(R.id.item_quantity_remove);
+
+        Button itemDeleteButton = (Button) rootView.findViewById(R.id.item_detail_delete);
+        Button itemReorderButton = (Button) rootView.findViewById(R.id.item_detail_reorder);
+
         // Set the TextViews
         itemPriceView.setText(itemPrice);
         itemQtyView.setText(itemQty);
         itemVendorNameView.setText(itemVendorName);
         itemVendorEmailView.setText(itemVendorEmail);
 
-
         // Get the app's files directory
         ContextWrapper cw = new ContextWrapper(getActivity());
         File filesDir = cw.getFilesDir();
         // Load the item image
         String itemImageDir = filesDir.toString();
-        String itemImagePath = itemImageDir + "/" + itemId ;
+        String itemImagePath = itemImageDir + "/" + itemId;
         Log.e(LOG_TAG, itemImageDir);
         Bitmap bitmap = BitmapFactory.decodeFile(itemImagePath);
         // Set the image view
         itemImageView.setImageBitmap(bitmap);
 
+        // Set the button click listeners
+        itemQuantityAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantityAdd(itemId);
+            }
+        });
+        itemQuantityRemoveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantityRemove(itemId);
+            }
+        });
+        itemReorderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemReorder(itemId);
+            }
+        });
+        itemDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemDelete(itemId);
+            }
+        });
+
         return rootView;
+    }
+
+    public void quantityAdd(int itemId){
+        modifyQuantityOnHand("add", itemId);
+    }
+    public void quantityRemove(int itemId){
+        modifyQuantityOnHand("remove", itemId);
+    }
+
+    public void modifyQuantityOnHand(String action, int itemId) {
+
+        Context context = getActivity().getApplicationContext();
+        CharSequence text;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast;
+
+        switch (action) {
+            case "add":
+                text = "added from" + itemId;
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+                break;
+            case "remove":
+                text =  "removed from" + itemId;
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    public void itemReorder(int itemId){
+        Context context = getActivity().getApplicationContext();
+        CharSequence text = "Reorder " + itemId;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void itemDelete(int itemId){
+        Context context = getActivity().getApplicationContext();
+        CharSequence text = "Delete " + itemId;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
